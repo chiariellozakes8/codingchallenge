adeee0580832cdc132fe58669535da2727f33d12
chiariellozakes8 <chiariellozakes8@gmail.com>
2025-03-15 19:09:22 +0000

    Add 9.rs

---
+++ src/9.rs
@@ -0,0 +1,13 @@
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("apple", 3);
+    map.insert("banana", 4);
+    map.insert("cherry", 5);
+
+    for (key, value) in &map {
+        println!("{}: {}", key, value);
+    }
+}
